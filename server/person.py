5c4179ee0a7bfcb92e7f87409a256837bb5d01ef
Ficolla <n.fabiani90@gmail.com>
2023-11-22 15:07:50 +0100

    refactor: add comments

--- server/person.py
+++ server/person.py
@@ -1,11 +1,18 @@
 class Person:
+    """
+    Represents a person, holds name, socket client and IP addr
+    """
     def __init__(self, addr, client):
         self.addr = addr
-        self.name = name
         self.client = client
-        set.name = None
+        self.name = None
 
     def set_name(self, name):
+        """
+        sets the persions name
+        :param name: str
+        :return: None
+        """
         self.name = name
 
     def __repr__(self):
